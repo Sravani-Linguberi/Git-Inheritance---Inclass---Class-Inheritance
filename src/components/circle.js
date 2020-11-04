41e9b17633c6172d86ede95c823516efcdc3f7e1
Sravani-Linguberi <sravani.linguberi369@gmail.com>
2020-11-04 16:39:02 +0530

    inheritance modified

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,15 @@
 
 
 // export class using module.exports
+import shape from './shape';
+export default class circle extends shape {
+    constructor() {
+      super();
+    }
+      calculateArea(){
+          console.log ("area of circle is 2pi *R^2 and color is:"+this.color);
+  
+          return (3.14*5*5);
+      }
+      
+  }
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,24 @@
-// declare class
+// // declare class
+// Acceptance Criteria
 
-// export class using module.exports
+// Create a base class called "Shape" with methods drawShape, calculateArea and color property that is initialised with a color.
+
+// In another module make a class called "Circle" that extends the "Shape" class and overrides the calculateArea method.
+
+// Write code for Shape class in shape.js and Circle in circle.js
+// // export class using module.exports
+
+export default class shape { // es6 export way
+    constructor() {
+        this.color = "red";
+    }
+    drawShape() {
+        console.log("Draw a shape");
+    }
+    calculateArea() {
+        console.log("don't know area of unknown shape");
+        return ;
+    }
+    
+}
+ 
